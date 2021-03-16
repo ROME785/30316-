2d5d131dc25f0788b07f1cc16ea05dda5f1ac914
ROME785 <47410405+ROME785@users.noreply.github.com>
2021-03-16 11:51:17 +0900

    .

---
+++ RoulletGame/Assets/RouletteController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            this.rotSpeed = 50.0f;
+            
+            }
+        if (Input.GetMouseButtonDown(1)){
+            this.rotSpeed = 0;
+
+        }
+        this.rotSpeed *= 0.987f; //속도가 서서히 줄어 들음.
+            transform.Rotate(0, 0, this.rotSpeed);
+        }
+    }
